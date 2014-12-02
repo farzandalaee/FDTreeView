7b8a0d1a148d4bd59dd08a1c33013cfe98679441
Farzan <farzan.dalaee@gmail.com>
2014-12-02 15:57:06 +0330

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
     //    CONFIG DATABASE
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'w$By44F^D^77');
+    define('DB_PASSWORD', 'pass');
     define('DB_DATABASE', 'FDTree');
 ?>
